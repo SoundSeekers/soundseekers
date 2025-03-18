1f0496b5b00c38e63ce638002d7fbff5a03cecb1
Marcel Ogorevc <1140@htl.rennweg.at>
2025-03-18 16:18:01 +0100

    new beginnings

--- gui/SoundSeekers/src/App.tsx
+++ gui/SoundSeekers/src/App.tsx
@@ -1,35 +1,10 @@
-import React, { Component } from 'react';
-import {View} from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+
 import windowStyles from './styles/WindowStyles';
-import OpenSheetMusicDisplay from 'opensheetmusicdisplay';
 
-class App extends Component {
-  constructor(props: {}) {
-    super(props);
-    // Don't call this.setState() here!
-    this.state = { file: "MuzioClementi_SonatinaOpus36No1_Part2.xml" };
-  }
-
-  handleClick(event: { target: { value: any; }; }) {
-    const file = event.target.value;
-    this.setState(state => state.file = file);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">OpenSheetMusicDisplay in React</h1>
-        </header>
-        <select onChange={this.handleClick.bind(this)}>
-          <option value="MuzioClementi_SonatinaOpus36No1_Part2.xml">Muzio Clementi: Sonatina Opus 36 No1 Part2</option>
-          <option value="Beethoven_AnDieFerneGeliebte.xml">Beethoven: An Die FerneGeliebte</option>
-        </select>
-        <OpenSheetMusicDisplay file={this.state.file} />
-      </div>
-    );
-  }
+function App(): React.JSX.Element {
+  return <View style={[windowStyles.window]}></View>;
 }
 
 export default App;
